cf704ec0fb375f83f95c3745b49ad793cb93abf4
alberttjin <albertjin@berkeley.edu>
2018-10-15 11:06:36 -0700

    user post and get works

--- oneliner/serializers.py
+++ oneliner/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 
+# class UserSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = User
+#         fields = ('id', 'first_name', 'last_name', 'email',)
+
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    username = serializers.CharField(
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    password = serializers.CharField()
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+             validated_data['password'])
+        return user
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email',)
+        fields = ('id', 'username', 'email', 'password')
--- oneliner/views.py
+++ oneliner/views.py
@@ -4,11 +4,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework import status
 
 # Create your views here.
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserList(APIView):
+    
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -18,4 +31,6 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 # class AddTask(APIView):
-    
+   
+#     def get(self, request, format=None):
+#         tasks = Task.objects.all()
